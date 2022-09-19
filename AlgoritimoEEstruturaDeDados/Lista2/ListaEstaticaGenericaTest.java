2fbbcbebb0e815a468385c459054c4391bcf004f
Felipe Babel <babelfelipe@gmail.com>
2022-09-19 19:46:49 -0300

    exercise list 2 - 08/18/2022

---
+++ AlgoritimoEEstruturaDeDados/Lista2/ListaEstaticaGenericaTest.java
@@ -0,0 +1,14 @@
+package Lista2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListaEstaticaGenericaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
