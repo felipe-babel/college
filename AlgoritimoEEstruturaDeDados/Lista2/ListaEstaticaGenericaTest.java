ea9fa37b60fa1261af4f06907aeff63e3a344582
Felipe Babel <felbabel@furb.br>
2022-10-06 17:12:24 -0300

    exercise list 2 (added tests) - 08/18/2022

--- AlgoritimoEEstruturaDeDados/Lista2/ListaEstaticaGenericaTest.java
+++ AlgoritimoEEstruturaDeDados/Lista2/ListaEstaticaGenericaTest.java
@@ -7,8 +7,26 @@ import org.junit.jupiter.api.Test;
 class ListaEstaticaGenericaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test10() {
+		ListaEstaticaGenerica<Integer> list = new ListaEstaticaGenerica<Integer>();
+		list.inserir(5);
+		list.inserir(10);
+		list.inserir(15);
+		list.inserir(20);
+		list.inverter();
+		assertEquals("20,15,10,5", list.toString());
+	}
+	
+	@Test
+	void test11() {
+		ListaEstaticaGenerica<Integer> list = new ListaEstaticaGenerica<Integer>();
+		list.inserir(5);
+		list.inserir(10);
+		list.inserir(15);
+		list.inserir(20);
+		list.inserir(25);
+		list.inverter();
+		assertEquals("25,20,15,10,5", list.toString());
 	}
 
 }
